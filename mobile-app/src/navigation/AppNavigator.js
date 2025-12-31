fd42591d8603a66704f555084613920d532d1650
Rudrappa838 <rudru7@gmail.com>
2025-12-31 11:44:16 +0530

    Build Fix: Installed missing lucide-react-native dependency and resolved nested NavigationContainer conflict

--- mobile-app/src/navigation/AppNavigator.js
+++ mobile-app/src/navigation/AppNavigator.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { useAuth } from '../contexts/AuthContext';
 import { ActivityIndicator, View, Text, TouchableOpacity } from 'react-native';
@@ -52,71 +51,69 @@ const AppNavigator = () => {
     }
 
     return (
-        <NavigationContainer>
-            <Stack.Navigator screenOptions={{ headerShown: false }}>
-                {!isAuthenticated ? (
-                    <>
-                        <Stack.Screen name="Login" component={LoginScreen} />
-                        <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
-                    </>
-                ) : (
-                    <>
-                        {/* Student Screens */}
-                        {user?.role === 'STUDENT' && (
-                            <>
-                                <Stack.Screen name="StudentDashboard" component={StudentDashboard} />
-                                <Stack.Screen name="Notifications" component={NotificationsScreen} />
-                                <Stack.Screen name="StudentAttendance" component={StudentAttendance} />
-                                <Stack.Screen name="StudentAcademics" component={StudentAcademics} />
-                                <Stack.Screen name="StudentFees" component={StudentFees} />
-                                <Stack.Screen name="StudentDoubts" component={StudentDoubts} />
-                                <Stack.Screen name="StudentLeaves" component={StudentLeaves} />
-                                <Stack.Screen name="StudentLibrary" component={StudentLibrary} />
-                                <Stack.Screen name="StudentHostel" component={StudentHostel} />
-                                <Stack.Screen name="StudentTransport" component={StudentTransport} />
-                                <Stack.Screen name="StudentCertificates" component={StudentCertificates} />
-                                <Stack.Screen name="StudentTimetable" component={PlaceholderScreen} />
-                                <Stack.Screen name="StudentAnnouncements" component={PlaceholderScreen} />
-                                <Stack.Screen name="StudentCalendar" component={PlaceholderScreen} />
-                            </>
-                        )}
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+            {!isAuthenticated ? (
+                <>
+                    <Stack.Screen name="Login" component={LoginScreen} />
+                    <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
+                </>
+            ) : (
+                <>
+                    {/* Student Screens */}
+                    {user?.role === 'STUDENT' && (
+                        <>
+                            <Stack.Screen name="StudentDashboard" component={StudentDashboard} />
+                            <Stack.Screen name="Notifications" component={NotificationsScreen} />
+                            <Stack.Screen name="StudentAttendance" component={StudentAttendance} />
+                            <Stack.Screen name="StudentAcademics" component={StudentAcademics} />
+                            <Stack.Screen name="StudentFees" component={StudentFees} />
+                            <Stack.Screen name="StudentDoubts" component={StudentDoubts} />
+                            <Stack.Screen name="StudentLeaves" component={StudentLeaves} />
+                            <Stack.Screen name="StudentLibrary" component={StudentLibrary} />
+                            <Stack.Screen name="StudentHostel" component={StudentHostel} />
+                            <Stack.Screen name="StudentTransport" component={StudentTransport} />
+                            <Stack.Screen name="StudentCertificates" component={StudentCertificates} />
+                            <Stack.Screen name="StudentTimetable" component={PlaceholderScreen} />
+                            <Stack.Screen name="StudentAnnouncements" component={PlaceholderScreen} />
+                            <Stack.Screen name="StudentCalendar" component={PlaceholderScreen} />
+                        </>
+                    )}
 
-                        {/* Teacher Screens */}
-                        {user?.role === 'TEACHER' && (
-                            <>
-                                <Stack.Screen name="TeacherDashboard" component={TeacherDashboard} />
-                                <Stack.Screen name="Notifications" component={NotificationsScreen} />
-                                <Stack.Screen name="TeacherAttendance" component={TeacherMyAttendance} />
-                                <Stack.Screen name="TeacherTimetable" component={TeacherTimetable} />
-                                <Stack.Screen name="TeacherSalary" component={TeacherSalary} />
-                                <Stack.Screen name="TeacherLeaves" component={TeacherLeaves} />
-                                <Stack.Screen name="TeacherDoubts" component={TeacherDoubts} />
-                                <Stack.Screen name="TeacherLibrary" component={TeacherLibrary} />
-                                <Stack.Screen name="TeacherTransport" component={TeacherTransport} />
-                                <Stack.Screen name="TeacherStudents" component={PlaceholderScreen} />
-                                <Stack.Screen name="TeacherDailyStatus" component={PlaceholderScreen} />
-                                <Stack.Screen name="TeacherAnnouncements" component={PlaceholderScreen} />
-                                <Stack.Screen name="TeacherCalendar" component={PlaceholderScreen} />
-                            </>
-                        )}
+                    {/* Teacher Screens */}
+                    {user?.role === 'TEACHER' && (
+                        <>
+                            <Stack.Screen name="TeacherDashboard" component={TeacherDashboard} />
+                            <Stack.Screen name="Notifications" component={NotificationsScreen} />
+                            <Stack.Screen name="TeacherAttendance" component={TeacherMyAttendance} />
+                            <Stack.Screen name="TeacherTimetable" component={TeacherTimetable} />
+                            <Stack.Screen name="TeacherSalary" component={TeacherSalary} />
+                            <Stack.Screen name="TeacherLeaves" component={TeacherLeaves} />
+                            <Stack.Screen name="TeacherDoubts" component={TeacherDoubts} />
+                            <Stack.Screen name="TeacherLibrary" component={TeacherLibrary} />
+                            <Stack.Screen name="TeacherTransport" component={TeacherTransport} />
+                            <Stack.Screen name="TeacherStudents" component={PlaceholderScreen} />
+                            <Stack.Screen name="TeacherDailyStatus" component={PlaceholderScreen} />
+                            <Stack.Screen name="TeacherAnnouncements" component={PlaceholderScreen} />
+                            <Stack.Screen name="TeacherCalendar" component={PlaceholderScreen} />
+                        </>
+                    )}
 
-                        {/* Staff Screens */}
-                        {['STAFF', 'DRIVER'].includes(user?.role) && (
-                            <>
-                                <Stack.Screen name="StaffDashboard" component={StaffDashboard} />
-                                <Stack.Screen name="Notifications" component={NotificationsScreen} />
-                                <Stack.Screen name="StaffMyAttendance" component={StaffMyAttendance} />
-                                <Stack.Screen name="StaffSalary" component={StaffSalary} />
-                                <Stack.Screen name="StaffTransport" component={StaffTransport} />
-                                <Stack.Screen name="DriverTracking" component={DriverTracking} />
-                                <Stack.Screen name="StaffAnnouncements" component={PlaceholderScreen} />
-                                <Stack.Screen name="StaffCalendar" component={PlaceholderScreen} />
-                            </>
-                        )}
-                    </>
-                )}
-            </Stack.Navigator>
-        </NavigationContainer>
+                    {/* Staff Screens */}
+                    {['STAFF', 'DRIVER'].includes(user?.role) && (
+                        <>
+                            <Stack.Screen name="StaffDashboard" component={StaffDashboard} />
+                            <Stack.Screen name="Notifications" component={NotificationsScreen} />
+                            <Stack.Screen name="StaffMyAttendance" component={StaffMyAttendance} />
+                            <Stack.Screen name="StaffSalary" component={StaffSalary} />
+                            <Stack.Screen name="StaffTransport" component={StaffTransport} />
+                            <Stack.Screen name="DriverTracking" component={DriverTracking} />
+                            <Stack.Screen name="StaffAnnouncements" component={PlaceholderScreen} />
+                            <Stack.Screen name="StaffCalendar" component={PlaceholderScreen} />
+                        </>
+                    )}
+                </>
+            )}
+        </Stack.Navigator>
     );
 };
 
