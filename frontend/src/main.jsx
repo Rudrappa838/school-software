11ff1d078830b600f0b9181af9b514326d7516b6
Rudrappa838 <rudru7@gmail.com>
2026-01-06 11:15:56 +0530

    Deployment Trigger: Version Check Log

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
+console.log('🚀 App Loaded | Version: 2026-01-06-Patch-1'); // Deployment Check
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
